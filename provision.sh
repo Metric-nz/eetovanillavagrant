4a99034f28c25f818ddb583ed4f3abeb6ae2da3b
Metric-nz <metric.nz@gmail.com>
2014-11-16 16:53:09 +1300

    Removed comments from provision.sh

--- provision.sh
+++ provision.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-# apt-get install -y apache2 mysql-server mysql-client php5 php5-mysql php5-gd
-#rm -rf /var/www
-#ln -fs /vagrant /var/www
 
 sudo debconf-set-selections <<< 'mysql-server \
  mysql-server/root_password password root'
